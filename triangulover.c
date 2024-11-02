9cec066ef02ee85c08e54232acf496056e0f7305
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-11-02 12:36:11 +0100

    3 triangulos

---
+++ triangulover.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float lado1, lado2, lado3;
+
+    printf("Digite o comprimento do lado 1: ");
+    scanf("%f", &lado1);
+    printf("Digite o comprimento do lado 2: ");
+    scanf("%f", &lado2);
+    printf("Digite o comprimento do lado 3: ");
+    scanf("%f", &lado3);
+
+    if ((lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1)) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            printf("O triangulo eh Equilatero.\n");
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            printf("O triângulo eh Isosceles.\n");
+        } else {
+            printf("O triangulo eh Escaleno.\n");
+        }
+    } else {
+        printf("Os valores inseridos nao formam um triangulo.\n");
+    }
+    system("PAUSE");
+    return 0;
+}
