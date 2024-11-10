fe6de80d9f7e31fdadbbeea087d41e137eefd58b
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-11-10 10:05:28 +0100

    feito o metodo Bublle sort no array

---
+++ Curso_Video/OrdernarVetorVideo.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#define TAM 6
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int numero[TAM];
+	int i, aux, contador;
+	printf("Entre com 6 números para preencher o array, e pressione enter após digitar cada um: \n");
+	//Entrada dos dados
+	for(i = 0; i < 6; i++){
+		scanf("%d", &numero[i]);
+	}
+	
+	printf("Ordem atual dos itens no array:\n");
+	//Mostrando a ordem atual
+	for(i = 0; i < 6; i++){
+		printf("%4d", numero[i]);
+	}
+	//Algoritmo de ordenação Bubblesort:
+	for(contador = 1; contador < TAM; contador++){
+		for(i = 0; i < TAM - 1; i++){
+			if(numero[i] > numero[i+1]){
+				aux = numero[i];
+				numero[i] = numero[i+1];
+				numero[i+1] = aux;
+			}
+		}
+	}
+	
+		printf("\n Elementos do array em ordem crescente:\n");
+		for(i = 0; i < TAM; i++){
+			printf("%4d", numero[i]);
+	}
+ 	return 0;
+}
